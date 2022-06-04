b77d08fb4d21ac728820ebd927d04caeabd76b3f
Wusi-Jeong <jws9595@naver.com>
2022-06-04 17:41:09 +0900

    2022. 06. 04

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const toggleBtn = document.querySelector('.navbar__toogleBtn');
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
